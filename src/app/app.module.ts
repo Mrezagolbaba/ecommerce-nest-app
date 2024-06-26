da6c02a4c7cb79992ddc9e3d2993f2efd635a8a0
Mohamadrez@ <m.reza.golbaba@gmail.com>
2024-06-26 08:58:30 +0300

    update app config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ProductModule } from './product/product.module';
+import { UserModule } from './user/user.module';
+import { CategoryModule } from './category/category.module';
+import { RepositoryModule } from './repositories/repository.module';
+import { ConfigModule } from '@nestjs/config';
+import { envSchema } from './utils';
 
-@Module({})
-export class AppModule {}
+@Module({
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+            validationSchema: envSchema
+        }),
+        ProductModule,
+        UserModule, 
+        CategoryModule, 
+        RepositoryModule
+    ],
+})
+export class AppModule { }
