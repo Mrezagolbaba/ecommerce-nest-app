180fcfb32b4d675ba4f839d39dc38250fe9960c4
Mohamadrez@ <m.reza.golbaba@gmail.com>
2024-06-27 08:56:50 +0300

    add validation pipline

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   const configService = app.get<ConfigService>(ConfigService);
   const port = configService.get('PORT');
   await app.listen(port);
